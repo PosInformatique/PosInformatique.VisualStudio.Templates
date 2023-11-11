8dea5469cc71d39ca24d07c0c527ae525b8d1931
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2023-11-11 23:54:39 +0100

    Add a Razor component item template with code-behind (fixes #3).

---
+++ src/VisualStudio.Templates.Files/RazorComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="$rootname$" company="$company$">
+//     Copyright (c) $company$. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$
+    {
+		public $safeitemname$()
+        {
+        }
+    }
+}
