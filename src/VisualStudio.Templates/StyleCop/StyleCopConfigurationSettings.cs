9043f6c9ef1640e7fc80f0bb96de1398855c361a
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2023-11-11 21:50:56 +0100

    Use the company specified in the stylecop.json file (fixes #2).

---
+++ src/VisualStudio.Templates/StyleCop/StyleCopConfigurationSettings.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="StyleCopConfigurationSettings.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.VisualStudio.Templates
+{
+    using System.Text.Json.Serialization;
+
+    public class StyleCopConfigurationSettings
+    {
+        public StyleCopConfigurationSettings(StyleCopConfigurationDocumentationRules documentationRules)
+        {
+            this.DocumentationRules = documentationRules;
+        }
+
+        [JsonPropertyName("documentationRules")]
+        public StyleCopConfigurationDocumentationRules DocumentationRules { get; }
+    }
+}
---
+++ src/VisualStudio.Templates/VisualStudio/ProjectInteropWrapper.cs
@@ -0,0 +1,43 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ProjectInteropWrapper.cs" company="P.O.S Informatique">
+//     Copyright (c) P.O.S Informatique. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace PosInformatique.VisualStudio.Templates
+{
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    public class ProjectInteropWrapper
+    {
+        private readonly IVsProject4 vsProject;
+
+        public ProjectInteropWrapper(IVsProject4 project)
+        {
+            this.vsProject = project;
+        }
+
+        public string GetDocumentFilePath(string name)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var result = this.vsProject.IsDocumentInProject(name, out var _, null, out var id);
+
+            if (result != 0)
+            {
+                return null;
+            }
+
+            // Gets the document moniker
+            result = this.vsProject.GetMkDocument(id, out var fileName);
+
+            if (result != 0)
+            {
+                return null;
+            }
+
+            return fileName;
+        }
+    }
+}
